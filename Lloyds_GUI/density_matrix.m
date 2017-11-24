8bd50b36277fd887e22b888d54efdff3d4d83a03
maxwellberkowitz <berkowitzmax@gmail.com>
2017-11-24 10:12:52 -0500

    adding .mat

--- Lloyds_GUI/density_matrix.m
+++ Lloyds_GUI/density_matrix.m
@@ -2,12 +2,15 @@
 sides = 5;
 partitionNumber = 5;
 prod = sides * partitionNumber;
-arr = zeros(prod);
-count = 0;
-for i = 0:prod-1
-    for j = 0:prod-1
-        if count > 3
-            arr(i,j)=1;
-        end
-    end
-end
+% arr = zeros(prod);
+% count = 0;
+% for i = 0:prod-1
+%     for j = 0:prod-1
+%         if count > 3
+%             arr(i,j)=1;
+%         end
+%     end
+% end
+arr = rand(25);
+arr(:) = round(1+arr(:)*2);
+save('C:\Users\Max Berkowitz\OneDrive - Queen''s University\2nd Year\APSC 200-293\P2\strikeforce\Lloyds_GUI\density_matrix.mat','arr');
