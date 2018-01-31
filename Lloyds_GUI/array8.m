54a0492909872dde4d50caf4935d96a58a63237e
maxwellberkowitz <15mab25@queensu.ca>
2018-01-31 18:33:49 -0500

    Cleaning files

--- Lloyds_GUI/array8.m
+++ Lloyds_GUI/array8.m
@@ -3,5 +3,5 @@ ar8(1,1)=2;
 ar8(3,1)=1;
 ar8(9,1)=3;
 ar8(18,14)=3;
-ar8(23,23)=10;
+ar8(23,23)=1;
 save('C:\Users\Max Berkowitz\OneDrive - Queen''s University\2nd Year\APSC 200-293\P2\strikeforce\Lloyds_GUI\array8.mat','ar8');
