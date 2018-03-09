008e3c47953d41186da464c841c6c3357568b5d2
maxwellberkowitz <15mab25@queensu.ca>
2018-03-08 21:35:15 -0500

    Cleaning

--- Lloyds_GUI/FUCK.m
+++
@@ -1,7 +0,0 @@
-FCK = zeros(25);
-FCK(1,1)=2;
-FCK(3,1)=1;
-FCK(9,1)=3;
-FCK(18,14)=3;
-FCK(23,23)=10;
-save('C:\Users\Max Berkowitz\OneDrive - Queen''s University\2nd Year\APSC 200-293\P2\strikeforce\Lloyds_GUI\FUCK.mat','FCK');
